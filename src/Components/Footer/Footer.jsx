33503fdd8ae56c6d64b64954db1166d0703b392a
rohanheydari <94110775+rohanheydari@users.noreply.github.com>
2025-12-18 11:59:31 -0800

    commit main files

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>@ 2024 Edusity,All Right Reserved </p>
+        <ul>
+            <li>Terms Of Services</li>
+            <li>Privacy Policy</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
